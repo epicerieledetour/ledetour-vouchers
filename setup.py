7e4cedc857c3e3c0bf268133b5414005ea10102e
Charles Flèche <charles.fleche@free.fr>
2022-10-01 17:59:35 -0400

    Add send_report.sh to setup.py scripts

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.0.0",
     include_package_data=True,
     packages=["app"],
+    scripts=["bin/send_report.sh"],
     python_requires=">=3.7",
     install_requires=["fastapi", "uvicorn[standard]", "Jinja2", "shortuuid"],
 )
