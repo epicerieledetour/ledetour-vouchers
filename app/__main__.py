30d20e14d027a2d5b456c6945e86f27adebb4723
Charles Flèche <charles.fleche@free.fr>
2023-08-05 19:48:47 -0400

    Remove stray debug log

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,4 @@
 import importlib
-import logging
 
 from .corecli.cli import parser
 
@@ -8,7 +7,6 @@ from . import modules
 for module in modules:
     name = f"{module.__name__}.cli"
     try:
-        logging.debug(f"Importing command line options from module {name}")
         importlib.import_module(name, "app")
     except ModuleNotFoundError:
         pass
