dd52d66dc551a79d5f60516c5a5a531f81de577a
Charles Flèche <charles.fleche@free.fr>
2023-09-13 14:12:04 -0400

    Print usage if no option is given to the cli

--- src/ldtvouchers/cli.py
+++ src/ldtvouchers/cli.py
@@ -51,13 +51,16 @@ def _users_read(args: argparse.Namespace, conn: sqlite3.Connection) -> None:
 
 
 def parse_args(args: Sequence[Text] | None = None) -> None:
-    ns = _build_parser().parse_args(args)
+    parser = _build_parser()
+    ns = parser.parse_args(args)
 
     # TODO: remove pragma no cover
     # For some reason the next two lines are not marked as
     # as coverage during testing, although executed
     if hasattr(ns, "command"):  # pragma: no cover
         ns.command(ns)  # pragma: no cover
+    else:  # pragma: no cover
+        parser.print_usage()  # pragma: no cover
 
 
 def _build_parser() -> argparse.ArgumentParser:
