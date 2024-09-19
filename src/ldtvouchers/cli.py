a008bb4b8a31c246a76b188ac855f3432e837fe8
Charles Flèche <charles.fleche@free.fr>
2024-09-18 21:56:17 -0400

    Add ssl arguments to serve cli

--- src/ldtvouchers/cli.py
+++ src/ldtvouchers/cli.py
@@ -262,8 +262,21 @@ def _server_serve(args: argparse.Namespace) -> None:
 
     import ldtvouchers.webapp
 
+    kwargs = {
+        "log_level": "info",
+        "host": args.host,
+        "port": args.port,
+    }
+    if args.ssl_keyfile and args.ssl_certfile:
+        kwargs.update(
+            {
+                "ssl_keyfile": args.ssl_keyfile,
+                "ssl_certfile": args.ssl_certfile,
+            }
+        )
+
     ldtvouchers.webapp.get_settings().debug = args.debug
-    uvicorn.run(ldtvouchers.webapp.app, port=args.port, log_level="info")
+    uvicorn.run(ldtvouchers.webapp.app, **kwargs)
 
 
 # Debug
@@ -457,7 +470,10 @@ def _build_parser() -> argparse.ArgumentParser:
     sub = subparsers.add_parser("server").add_subparsers()
 
     par = sub.add_parser("serve")
+    par.add_argument("--host", default="127.0.0.1", help="Web server host")
     par.add_argument("--port", default=8080, type=int, help="Web server port")
+    par.add_argument("--ssl_certfile", help="Web server SSL certfile")
+    par.add_argument("--ssl_keyfile", help="Web server SSL keyfile")
     par.set_defaults(command=_server_serve)
 
     # debug
