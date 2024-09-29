6da09550b7c0d40757882f8c3a4af8d4510524ce
Charles Flèche <charles.fleche@free.fr>
2024-09-29 00:56:32 -0400

    Fix vouchers template

--- src/ldtvouchers/gen.py
+++ src/ldtvouchers/gen.py
@@ -126,8 +126,10 @@ def emission_vouchers(
     # Convert the verso SVG to a temporary PDF
 
     verso_pdf_path = tmpdir / "verso.pdf"
-    with verso_pdf_path.open("wb") as fd:
-        _svg2pdf(url=str(_VOUCHERS_VERSO_SVG_PATH), write_to=fd)
+    with verso_pdf_path.open("wb") as fd, _VOUCHERS_VERSO_SVG_PATH.open(
+        "r"
+    ) as verso_fd:
+        _svg2pdf(file_obj=verso_fd, write_to=fd)
 
     # Initialize the jinja environment
 
