eca8640ec96f1064a89993da55bca6f78f7c4395
Charles Flèche <charles.fleche@free.fr>
2024-10-23 00:25:09 -0400

    Fix emailreport time

--- src/ldtvouchers/db/emission_emailreport.sql
+++ src/ldtvouchers/db/emission_emailreport.sql
@@ -1,9 +1,9 @@
 SELECT
     u.description AS user, 
-    time(cashedin_utc, "localtime") AS localtime,
+    time(v.cashedin_utc, "localtime") AS localtime,
     tv.token AS token
 FROM vouchers v
 JOIN tokens tv ON v.voucherid = tv.idintable AND tv.tablename = "vouchers"
 JOIN users u ON v.cashedin_by = u.userid
-WHERE cashedin_utc > date(:date_utc, "-1 day") AND cashedin_utc <= date(:date_utc)
+WHERE v.cashedin_utc > date(:date_utc, "-1 day") AND v.cashedin_utc <= date(:date_utc)
 ORDER BY u.label ASC, v.cashedin_utc ASC;
