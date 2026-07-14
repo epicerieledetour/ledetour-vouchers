9fe38bbdc428fabb1d038e319f518ded21100425
charlesfleche <charles.fleche@free.fr>
2026-07-13 21:14:30 -0400

    Make executable by uv / uvx

--- src/ldtvouchers/__main__.py
+++ src/ldtvouchers/__main__.py
@@ -2,4 +2,11 @@
 
 from . import cli  # pragma: no cover
 
-cli.parse_args()  # pragma: no cover
+# Function here is just for pyproject.toml
+# projects.scripts
+def main():
+    cli.parse_args()  # pragma: no cover
+
+if __name__ == "__main__":
+    main()
+
